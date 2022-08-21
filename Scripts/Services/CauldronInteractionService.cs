ed6cfc9c16ee60560501934b34caafc17235cb17
Andrew Fahlgren <andrewfahlgren@gmail.com>
2022-08-20 18:25:16 -0700

    Added basic shell methods

--- Scripts/Services/CauldronInteractionService.cs
+++ Scripts/Services/CauldronInteractionService.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using PotionCraft.ManagersSystem;
+using PotionCraft.ObjectBased.InteractiveItem.Vacuuming;
+using PotionCraft.ObjectBased.Potion;
+using PotionCraft.ScriptableObjects;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace PotionCraftPourBackIn.Scripts.Services
 {
@@ -9,5 +14,46 @@ namespace PotionCraftPourBackIn.Scripts.Services
     /// </summary>
     public static class CauldronInteractionService
     {
+        public static bool AcceptPotionCollision(Collider2D other)
+        {
+            var potionItem = other.GetComponentInParent<PotionItem>();
+            if (potionItem == null) return true;
+
+            if (Managers.Potion.potionCraftPanel.IsPotionBrewingStarted()) return false;
+
+            var potion = potionItem.inventoryItem as Potion;
+
+            PotionService.ContinueBrewingFromPotion(potion);
+            AnimateSplash(potion);
+            DeletePotion(potion);
+
+            return false;
+        }
+
+        public static void AnimateSplash(Potion potion)
+        {
+            //check out how WaterZone.OnTriggerStay2D does it
+        }
+
+        public static void DeletePotion(Potion potion)
+        {
+            //here we need to remove the potion from the game world
+        }
+
+
+
+        public static bool AllowCauldronCollision(Collider2D target, bool ignore)
+        {
+            if (!ignore) return true;
+            var cauldronVaccum = Managers.Ingredient.cauldron.throwVacuuming;
+            var cauldronCollider = typeof(ThrowVacuuming).GetField("colliderPhysics").GetValue(cauldronVaccum) as Collider2D;
+            if (target == cauldronCollider)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
     }
 }
--- Scripts/Services/PotionService.cs
+++ Scripts/Services/PotionService.cs
@@ -1,8 +1,13 @@
-﻿using PotionCraft.ManagersSystem.Potion;
+﻿using PotionCraft.ManagersSystem;
+using PotionCraft.ManagersSystem.Potion;
+using PotionCraft.ObjectBased.InteractiveItem.Vacuuming;
+using PotionCraft.ObjectBased.Potion;
+using PotionCraft.ObjectBased.RecipeMap.RecipeMapItem.PotionEffectMapItem;
 using PotionCraft.ScriptableObjects;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace PotionCraftPourBackIn.Scripts.Services
 {
@@ -11,14 +16,48 @@ namespace PotionCraftPourBackIn.Scripts.Services
     /// </summary>
     public static class PotionService
     {
-        public static void CopyImportantInfoToPotionInstance(Potion result, Potion instance)
+
+        public static void ContinueBrewingFromPotion(Potion potion)
         {
-            throw new NotImplementedException();
+            var potionFromPanel = potion.potionFromPanel;
+            var potionBase = potionFromPanel.potionUsedComponents.Count == 0 
+                                ? Managers.RecipeMap.currentMap.potionBase 
+                                : PotionBase.GetByName(potionFromPanel.potionUsedComponents[0].componentName);
+            potionFromPanel.ApplyPotionToCurrentPotion(potionBase); //TODO make sure we don't remove ingredients when we do this
+            //TODO make sure we update enablement on recipe buttons when we do this
+            foreach (var collectedPotionEffect in Managers.Potion.collectedPotionEffects)
+            {
+                if (!(collectedPotionEffect == null))
+                {
+                    foreach (var potionEffectsOn in Managers.RecipeMap.currentMap.potionEffectsOnMap)
+                    {
+                        if (potionEffectsOn.effect == collectedPotionEffect)
+                        {
+                            potionEffectsOn.Status = PotionEffectStatus.Collected;
+                            break;
+                        }
+                    }
+                }
+            }
         }
 
-        public static void CopyImportantInfoToPotionInstance(Potion result, PotionManager instance)
+        public static void CopyImportantInfoToPotionInstance(Potion copyTo, PotionManager instance)
         {
-            throw new NotImplementedException();
+            var copyFrom = instance.potionCraftPanel.GetCurrentPotion();
+            CopyImportantInfoToPotionInstance(copyTo, copyFrom);
+        }
+
+        /// <summary>
+        /// This method copies important information to the potion that is normally lost unless the potion is saved as a recipe
+        /// This is important so we can use the potion just like a recipe later
+        /// </summary>
+        public static void CopyImportantInfoToPotionInstance(Potion copyTo, Potion copyFrom)
+        {
+            var recipeMarks = copyTo.potionFromPanel.recipeMarks;
+            copyFrom.potionFromPanel.recipeMarks.ForEach(m => recipeMarks.Add(m.Clone()));
+            copyTo.potionFromPanel.collectedPotionEffects = copyFrom.potionFromPanel.collectedPotionEffects;
+
+            //TODO also copy other things from the SerializedPotionFromPanel like the current path and who knows what else
         }
     }
 }
